5e085b033450ebc60b1e6c1a476eaa19f5ebd84b
Chansoo Park <chansoopark@Chansoos-MacBook-Air.local>
2023-11-23 02:53:08 +0900

    Fix: something

--- src/commons/theme/globalStyles.theme.ts
+++ src/commons/theme/globalStyles.theme.ts
@@ -6,9 +6,13 @@ export const globalStyles = {
       padding: 0,
       borderRadius: "16px",
     },
+    "html, body": {
+      height: "100dvh", // 전체 높이를 설정
+      margin: 0,
+      padding: 0,
+    },
     body: {
       overflow: "hidden",
-      screen: "100dvh",
     },
   },
 };
