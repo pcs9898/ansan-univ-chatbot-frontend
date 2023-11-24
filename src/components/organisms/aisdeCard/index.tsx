ef50532f356c71f77a3d03b9614950581643f9ca
Chansoo Park <chansoopark@Chansoos-MacBook-Air.local>
2023-11-25 00:39:14 +0900

    Fix: something

--- src/components/organisms/aisdeCard/index.tsx
+++ src/components/organisms/aisdeCard/index.tsx
@@ -53,8 +53,8 @@ export default function AsideCard() {
           bgColor="#D2D7F4"
           imageSettings={{
             src: "/avatarImage.svg",
-            width: 30,
-            height: 30,
+            width: 40,
+            height: 40,
             excavate: false,
           }}
         />
