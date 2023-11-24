90260a25a22e3e6fccf382c333290b2a0d986de2
Chansoo Park <chansoopark@Chansoos-MacBook-Air.local>
2023-11-25 00:27:03 +0900

    Fix: something

--- src/components/molecules/loadingMessage/index.tsx
+++ src/components/molecules/loadingMessage/index.tsx
@@ -4,13 +4,15 @@ import { PulseLoader } from "react-spinners";
 export default function LoadingMessage() {
   return (
     <Card
-      p="1rem"
-      pl="0px"
+      py="0.625rem"
+      px="0.75rem"
       variant="unstyled"
       colorScheme="blue"
       w="max-content"
+      h="2.5rem"
       display="flex"
       alignItems="center"
+      justifyContent="center"
     >
       <PulseLoader size={8} speedMultiplier={0.8} color="#9A9A9A" />
     </Card>
--- src/components/organisms/aisdeCard/index.tsx
+++ src/components/organisms/aisdeCard/index.tsx
@@ -53,8 +53,8 @@ export default function AsideCard() {
           bgColor="#D2D7F4"
           imageSettings={{
             src: "/avatarImage.svg",
-            width: 50,
-            height: 50,
+            width: 40,
+            height: 40,
             excavate: false,
           }}
         />
