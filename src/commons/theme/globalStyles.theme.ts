917e09608569811dd2417ce09867d1618725578b
Chansoo Park <chansoopark@Chansoos-MacBook-Air.local>
2023-11-25 23:33:21 +0900

    Fix: something

--- src/commons/theme/globalStyles.theme.ts
+++ src/commons/theme/globalStyles.theme.ts
@@ -6,6 +6,13 @@ export const globalStyles = {
       padding: 0,
       borderRadius: "16px",
       "-webkit-tap-highlight-color": "transparent",
+      "@media (hover: hover)": {
+        "*": {
+          "&:hover": {
+            opacity: 1,
+          },
+        },
+      },
     },
     "html, body": {
       height: "100dvh", // 전체 높이를 설정
