6cdd5e04826365bf58241e8dbee5115906a9d51b
Chansoo Park <chansoopark@Chansoos-MacBook-Air.local>
2023-11-25 22:57:28 +0900

    Fix: something

--- src/commons/theme/customTheme.theme.ts
+++ src/commons/theme/customTheme.theme.ts
@@ -1,4 +1,8 @@
-import { extendTheme } from "@chakra-ui/react";
+import {
+  extendTheme,
+  theme as defaultTheme,
+  StyleFunctionProps,
+} from "@chakra-ui/react";
 import { globalStyles } from "./globalStyles.theme";
 import chakraColorModeConfig from "./config.theme";
 import { cardTheme } from "./components/card.theme";
@@ -10,5 +14,16 @@ export const customTheme = extendTheme({
   colors: customColors,
   components: {
     Card: cardTheme,
+    Button: {
+      variants: {
+        solid: (props: StyleFunctionProps) => ({
+          "@media(hover: none)": {
+            _hover: {
+              bg: defaultTheme.components.Button.variants.solid(props).bg,
+            },
+          },
+        }),
+      },
+    },
   },
 });
