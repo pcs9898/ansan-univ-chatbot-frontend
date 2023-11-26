92c09b4c3127926896f0aee080d3ae192d1c83bb
Chansoo Park <chansoopark@Chansoos-MacBook-Air.local>
2023-11-26 19:53:13 +0900

    Fix: something

--- src/components/organisms/customCards/index.tsx
+++ src/components/organisms/customCards/index.tsx
@@ -47,7 +47,7 @@ function CustomCards({ cardsProps, isLoading }: ICardsProps) {
                         height: "8px",
                       },
                       "::-webkit-scrollbar-track": {
-                        background: "#f1f1f1",
+                        background: "transparent",
                       },
                       "::-webkit-scrollbar-thumb": {
                         background: "#888",
